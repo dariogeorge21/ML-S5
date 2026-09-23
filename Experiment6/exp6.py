031730f019b1fc4ed1f01afbedeabcd6621e55a9
Dario George <edu.dariogeorge21@gmail.com>
2026-09-23 12:47:24 +0530

    feat: implement customer segmentation and entropy calculation in Experiment 6

--- Experiment6/exp6.py
+++ Experiment6/exp6.py
@@ -9,3 +9,70 @@ df = df[~df["InvoiceNo"].astype(str).str.startswith("C")]
 df = df[(df["Quantity"]>0) & (df["UnitPrice"] > 0)]
 df["TotalAmount"] = df["Quantity"] * df["UnitPrice"]
 
+customer = df.groupby("CustomerID").agg(
+    Frequency = ("InvoiceNo","nunique"),
+    TotalQuantity = ("Quantity","sum"),
+    UniqueProducts = ("StockCode","nunique"),
+    Monetary = ("TotalAmount","sum")
+).reset_index()
+
+customer["AvgBasketValue"] = (
+    customer["Monetary"] / customer["Frequency"]
+)
+
+customer["Segment"] = pd.qcut(
+    customer["Monetary"],
+    q=3,
+    labels=["Low","Medium","High"]
+)
+
+print("\nCustomer Segments:")
+print(customer["Segment"].value_counts())
+
+features = {
+    "Frequency",
+    "TotalQuantity",
+    "UniqueProducts",
+    "Monetary",
+    "AvgBasketValue"
+}
+
+for feature in features:
+    customer[feature] = pd.cut(
+        customer[feature],
+        bins = 3,
+        labels = ["Low", "Medium", "High"],
+        include_lowest = True
+    )
+
+def entropy(data):
+    counts = data["Segment"].value_counts()
+    total = len(data)
+    ent = 0
+    for count in counts:
+        p = count / total
+        if p > 0:
+            ent -= p * np.log2(p)
+        return ent
+
+
+total_entropy = entropy(customer)
+print("\nOverall Entropy =", round(total_entropy, 4))
+
+results = []
+for feature in features:
+    weighted_entropy = 0
+
+    for value in customer[feature].dropna().unique():
+        subset = customer[customer[feature] == value]
+        subset_entropy = entropy(subset)
+        weight = len(subset) / len(customer)
+        weight_entropy += weight * subset_entropy
+        information_gain = total_entropy-weighted_entropy
+
+        results.append([
+            feature,
+            weighted_entropy,
+            information_gain
+        ])
+
